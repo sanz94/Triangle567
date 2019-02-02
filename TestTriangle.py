97fd43eda50ed24908cb764073c191f2abcec128
Sanjeev Rajasekaran <sanjeev95@rocketmail.com>
2019-02-02 01:33:27 -0500

    Fixed bugs

--- TestTriangle.py
+++ TestTriangle.py
@@ -26,6 +26,33 @@ class TestTriangles(unittest.TestCase):
     def testEquilateralTriangles(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
 
+    #added test cases
+
+    def testInput(self):
+        self.assertNotEqual(classifyTriangle(300, 300, 300), 'InvalidInput')
+
+    def testNegatives(self):
+        self.assertEqual(classifyTriangle(-1, 1, 1), 'InvalidInput')
+        self.assertEqual(classifyTriangle(1, -1, 1), 'InvalidInput')
+
+    def testFloat(self):
+        self.assertNotEqual(classifyTriangle(1.5, 1.5, 1.5), 'InvalidInput')
+
+    def testValidTriangle(self):
+        self.assertNotEqual(classifyTriangle(2, 3, 4), 'NotATriangle')
+        self.assertNotEqual(classifyTriangle(3, 2, 4), 'NotATriangle')
+        self.assertEqual(classifyTriangle(2, 3, 10), 'NotATriangle')
+
+    def testTriangleType(self):
+        self.assertNotEqual(classifyTriangle(2, 2, 4), 'Equilateral')
+        self.assertEqual(classifyTriangle(2, 2, 2), 'Equilateral')
+        self.assertNotEqual(classifyTriangle(2, 2, 4), 'Right')
+        self.assertEqual(classifyTriangle(3, 4, 5), 'Right')
+        self.assertNotEqual(classifyTriangle(2, 4, 2), 'Scalene')
+        self.assertEqual(classifyTriangle(2, 3, 4), 'Scalene')
+        self.assertNotEqual(classifyTriangle(2, 4, 3), 'Isoceles')
+        self.assertEqual(classifyTriangle(2, 2, 4), 'Isoceles')
+
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
